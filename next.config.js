7a72a350ac4f1224aab52fc904b86f3008c58a3c
GokulKannur <gokulvursuline@protonmail.com>
2025-09-10 11:30:52 +0530

    new features

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // config options here
+  // Add this async redirects() function
   async redirects() {
     return [
+      {
+        source: '/calculators/area-calculator',
+        destination: '/calculators/area-calculator', // Redirects to the same URL to force a correct lookup
+        permanent: true,
+      },
       {
         source: '/calculators/area-calculator-',
         destination: '/calculators/area-calculator',
